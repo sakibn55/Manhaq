4b07de38aa695cafbadf6053938b1f767d7e9955
Nazmus Sakib <sakibn55@gmail.com>
2018-03-11 04:31:55 +0600

    some ui changes

--- js/main.js
+++ js/main.js
@@ -1,96 +1,94 @@
-$(document).ready(function(){
-  // Add scrollspy to <body>
-  $('body').scrollspy({target: ".navbar", offset: 0});   
+$(document).ready(function () {
+    // Add scrollspy to <body>
+    $('body').scrollspy({target: ".navbar", offset: 0});
 
-  // Add smooth scrolling on all links inside the navbar
-  $("#navigation a").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+    // Add smooth scrolling on all links inside the navbar
+    $("#navigation a").on('click', function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+            // Store hash
+            var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-		
-        window.location.hash = hash;
-      });
-    }  // End if
-  });
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+
+                window.location.hash = hash;
+            });
+        }  // End if
+    });
 });
 
-	
-	
-	
-	var container = document.querySelector('[data-ref="container"]');
-            var inputSearch = document.querySelector('[data-ref="input-search"]');
-            var keyupTimeout;
 
-            var mixer = mixitup(container, {
-                animation: {
-                    duration: 350
-                },
-                callbacks: {
-                    onMixClick: function() {
-                        // Reset the search if a filter is clicked
+var container = document.querySelector('[data-ref="container"]');
+var inputSearch = document.querySelector('[data-ref="input-search"]');
+var keyupTimeout;
 
-                        if (this.matches('[data-filter]')) {
-                            inputSearch.value = '';
-                        }
-                    }
-                }
-            });
-			
-			// Set up a handler to listen for "keyup" events from the search input
+var mixer = mixitup(container, {
+    animation: {
+        duration: 350
+    },
+    callbacks: {
+        onMixClick: function () {
+            // Reset the search if a filter is clicked
 
-            inputSearch.addEventListener('keyup', function() {
-                var searchValue;
-
-                if (inputSearch.value.length < 3) {
-                    // If the input value is less than 3 characters, don't send
-
-                    searchValue = '';
-                } else {
-                    searchValue = inputSearch.value.toLowerCase().trim();
-                }
-
-                // Very basic throttling to prevent mixer thrashing. Only search
-                // once 350ms has passed since the last keyup event
-
-                clearTimeout(keyupTimeout);
-
-                keyupTimeout = setTimeout(function() {
-                    filterByString(searchValue);
-                }, 350);
-            });
-        
-            /**
-             * Filters the mixer using a provided search string, which is matched against
-             * the contents of each target's "class" attribute. Any custom data-attribute(s)
-             * could also be used.
-             *
-             * @param  {string} searchValue
-             * @return {void}
-             */
-
-            function filterByString(searchValue) {
-                if (searchValue) {
-                    // Use an attribute wildcard selector to check for matches
-
-                    mixer.filter('[class*="' + searchValue + '"]');
-                } else {
-                    // If no searchValue, treat as filter('all')
-
-                    mixer.filter('all');
-                }
+            if (this.matches('[data-filter]')) {
+                inputSearch.value = '';
             }
+        }
+    }
+});
+
+// Set up a handler to listen for "keyup" events from the search input
+
+inputSearch.addEventListener('keyup', function () {
+    var searchValue;
+
+    if (inputSearch.value.length < 3) {
+        // If the input value is less than 3 characters, don't send
+
+        searchValue = '';
+    } else {
+        searchValue = inputSearch.value.toLowerCase().trim();
+    }
+
+    // Very basic throttling to prevent mixer thrashing. Only search
+    // once 350ms has passed since the last keyup event
+
+    clearTimeout(keyupTimeout);
+
+    keyupTimeout = setTimeout(function () {
+        filterByString(searchValue);
+    }, 350);
+});
+
+/**
+ * Filters the mixer using a provided search string, which is matched against
+ * the contents of each target's "class" attribute. Any custom data-attribute(s)
+ * could also be used.
+ *
+ * @param  {string} searchValue
+ * @return {void}
+ */
+
+function filterByString(searchValue) {
+    if (searchValue) {
+        // Use an attribute wildcard selector to check for matches
+
+        mixer.filter('[class*="' + searchValue + '"]');
+    } else {
+        // If no searchValue, treat as filter('all')
+
+        mixer.filter('all');
+    }
+}
 
 	
 	
